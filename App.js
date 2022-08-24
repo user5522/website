6a48a5338b7ab5b69f97e5c54ef0be4b23646389
Username * <oueslatim268@gmail.com>
2022-08-24 22:07:29 +0100

    react stuff

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+      <Text style={styles.title}><a href="https://github.com/user5522" rel="noreferrer">user5522</a></Text>
     </View>
   );
 }
@@ -13,8 +12,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 25,
+    alignItems: "stretch",
+    position: "absolute",
+    top: 25,
+    left: 25,
   },
 });
