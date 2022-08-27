c74ab54146f62c2a427d5f8a4e07f7a1e23c7247
Username * <65370936+user5522@users.noreply.github.com>
2022-08-27 16:02:36 -0700

    maybe she was the problem...

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/user5522.github.io/",
+  base: "https://user5522.github.io/",
   plugins: [react()],
 });
