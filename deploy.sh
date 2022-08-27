6b39e4cbb86b1c55cefa22b5ee940170179542f9
User5522 <oueslatim268@gmail.com>
2022-08-27 16:29:52 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:User5522/user5522.github.io.git main
+git push -f git@github.com/User5522/user5522.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:User5522/user5522.github.io.git main:gh-pages
