037578a6e247fdd5ecaa29e2237ad8f7fa1ea2cd
User5522 <oueslatim268@gmail.com>
2022-08-28 17:17:48 +0100

    add 404 page route (maybe)

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <React.StrictMode>
     <main style={{ padding: "1rem 0" }}>
-      <h2 className="text-7x1">404 not found kys</h2>
+      <h2 className="text-7x1">
+        404 not found, <br /> please kys asap & thanks for your understanding.
+      </h2>
     </main>
   </React.StrictMode>,
   document.getElementById("root")
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       input: {
         main: resolve(root, "index.html"),
         vbot: resolve(root, "vbot", "index.html"),
+        NotFound: resolve(root, "*", "index.html"),
       },
     },
   },
