af6cc3e33629ba4794ed57ef50e52d26e724ed06
User5522 <oueslatim268@gmail.com>
2022-08-29 22:31:09 +0100

    fixes and enhancements

--- src/components/closeButton.svelte
+++ src/components/closeButton.svelte
@@ -1,7 +1,11 @@
 <script>
-	export let onClose;
+  export let onClose;
 </script>
 
-<button on:click={onClose} class="absolute z-50 right-10 -top-0"
-	><img src="/close.svg" alt="Close popup" class="bg-black" /></button
+<button on:click={onClose} class="absolute z-50 right-10 top-5"
+  ><img
+    src="/close.svg"
+    alt="Close popup"
+    class="rounded-full bg-dark_light p-2 hover:bg-red-900 hover:scale-110 active:scale-90 duration-200"
+  /></button
 >
