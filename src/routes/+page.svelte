65494b67fb3abfaeb05fcf466721432252b36d05
User5522 <oueslatim268@gmail.com>
2022-09-01 00:50:28 +0100

    more responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,10 @@
   import WavesStart from "../components/waves_start.svelte";
   import { writable } from "svelte/store";
   import Modal, { bind } from "svelte-simple-modal";
-  import Popup from "../components/crimes.svelte";
+  import Popup from "./Accusations/+page.svelte";
   import CloseButton from "../components/closeButton.svelte";
   import Footer from "../components/footer.svelte";
+  import Subtitle from "../components/subtitle.svelte";
 
   const modal = writable(null);
   const showModal = () => modal.set(bind(Popup, "test"));
@@ -19,20 +20,22 @@
 </div>
 <div class="pt-28 bg-black">
   <div class="flex flex-col items-center justify-center pb-20">
-    <div class="flex flex-row">
+    <div class="flex flex-row items-baseline">
       <p class="ml-16 text-4xl">User5522</p>
-      <p class="mt-6 text-xs">/Username5522</p>
+      <p class="text-xs">/Username5522</p>
     </div>
-    <div class="pb-10 text-xl">professional peepeepoopoo inspector</div>
-    <div class="pb-10">
-      Welcome to my website, a place where i put everything i failed at making
+    <div><Subtitle /></div>
+    <div class="p-10">
+      Welcome to my website, a place where I publish everything I failed at
+      making
     </div>
-    <div>
+    <div class="pl-10 pr-10">
       I am NOT wanted in several countries for commiting any sort of federal
-      crimes.
+      crime.
     </div>
-    <div class="pb-10">
-      I am an innocent individual, and I didn't commit any of these crimes:
+    <div class="pb-10 pr-10 pl-10">
+      I am an innocent individual, and I definitely didn't commit any of these
+      crimes:
     </div>
     <div>
       <!-- svelte-ignore a11y-missing-attribute -->
@@ -42,15 +45,22 @@
         closeButton={CloseButton}
         styleContent={{ backgroundColor: "#121212", borderRadius: "0.75rem" }}
       >
-        <a>
+        <a class="mr-2">
           <button
-            class="p-5 text-blue-300 duration-200 bg-blue-800 rounded-xl hover:scale-110 active:scale-90"
+            class="sm:p-5 p-4 sm:text-base text-sm text-blue-200 duration-200 bg-blue-800 rounded-xl hover:scale-110 active:scale-90"
             on:click={showModal}
           >
             View false accusations
           </button>
         </a>
+        <a href="/Accusations"
+          ><button
+            class="sm:p-5 p-4 sm:text-base text-sm hover:bg-dark_darker rounded-xl hover:scale-110 active:scale-90 duration-200"
+            >View in different page</button
+          ></a
+        >
       </Modal>
     </div>
   </div>
+  <Footer />
 </div>
