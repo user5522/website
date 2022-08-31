a5b531ef90c5e4cd75cde3ee13e30189d20055b9
User5522 <oueslatim268@gmail.com>
2022-09-01 00:49:07 +0100

    re-add the footer and make it responsive

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,6 +1,9 @@
-<div id="footer" class="bg-true_gray w-full h-56 relative top-0 mt-20">
-  <div class="">
-    <img src="/Logo_monochrome.svg" alt="monochrome logo" class="" />
-    <div>User5522</div>
+<div id="footer" class="bg-true_gray w-full h-64 relative top-0 mt-20">
+  <div class="flex flex-row items-center">
+    <img
+      src="/Logo_monochrome.svg"
+      alt="monochrome logo"
+      class="sm:mt-10 sm:ml-10 mt-5 ml-5 sm:h-32 h-28 bg-light_true_gray rounded-full"
+    />
   </div>
 </div>
