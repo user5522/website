9d22b5ed89c0215d4a5896105c0aa4837cfb58c4
User5522 <oueslatim268@gmail.com>
2022-09-01 00:51:12 +0100

    make the navbar as responsive as i can

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,25 +1,35 @@
-<div class="fixed top-0 z-50 w-screen p-3">
-  <nav id="navbar" class="flex flex-row items-center p-5 rounded-xl bg-dark">
-    <div class="flex flex-row items-center gap-2 duration-300" id="logo">
-      <img
-        src="/Logo_flat.svg"
-        class="items-center h-10 rounded-full bg-dark_light"
-        alt="logo"
-      />
+<div
+  class="fixed top-0 z-50 w-screen p-3 flex flex-col justify-center align-middle"
+>
+  <nav
+    id="navbar"
+    class="flex flex-row items-center sm:p-5 p-4 rounded-xl bg-dark"
+  >
+    <div
+      class="flex flex-row items-center sm:gap-2 gap-1 duration-300"
+      id="logo"
+    >
+      <button class="active:rotate-158 duration-300">
+        <img
+          src="/Logo_flat.svg"
+          class="items-center sm:h-10 h-9 rounded-full bg-dark_light"
+          alt="logo"
+        />
+      </button>
       <a href="/">
         <button
           id="logo_text"
-          class="p-3 text-xl font-semibold duration-200 rounded-lg hover:bg-dark_light hover:scale-110 active:scale-90"
+          class="sm:p-3 p-2 sm:text-xl text-base font-semibold duration-200 rounded-lg hover:bg-dark_light hover:scale-110 active:scale-90"
           alt="logo"
         >
           User5522
         </button>
       </a>
     </div>
-    <div id="largeScreenItems" class="absolute right-10">
+    <div class="absolute right-10">
       <a href="/Vbot">
         <button
-          class="p-3 hover:scale-110 text-xl active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
+          class="sm:p-3 p-2 hover:scale-110 sm:text-xl text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
         >
           Vbot
         </button>
@@ -27,47 +37,19 @@
       <a href="/About">
         <button
           href="/About"
-          class="p-3 hover:scale-110 text-xl active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
+          class="sm:p-3 p-2 hover:scale-110 sm:text-xl text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
         >
           About
         </button>
       </a>
-    </div>
-    <script>
-      let y = 0;
-
-      function toggleBurgerMenu() {
-        var x = document.getElementById("BurgerMenu");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
-    </script>
-    <div class="sm:hidden">
-      <div class="flex items-center">
+      <a href="/Projects">
         <button
-          onClick="toggleBurgerMenu()"
-          class="absolute p-3 text-xl duration-100 rounded-lg hover:bg-dark_light hover:scale-110 active:scale-90 right-10"
+          href="/Projects"
+          class="sm:p-3 p-2 hover:scale-110 sm:text-xl text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
         >
-          <img src="/burger.svg" alt="hamburger menu" />
+          Projects
         </button>
-      </div>
-      <div id="BurgerMenu" class="">
-        <div class="w-40 h-96">
-          <div>Test</div>
-        </div>
-      </div>
+      </a>
     </div>
   </nav>
 </div>
-
-<!-- svelte-ignore a11y-missing-attribute -->
-<style>
-  @media only screen and (max-width: 640px) {
-    #largeScreenItems {
-      display: none;
-    }
-  }
-</style>
