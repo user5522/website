882951a1e3b59cdd73a9317a1589fc274e81647c
User5522 <oueslatim268@gmail.com>
2022-09-01 00:50:52 +0100

    add a change-able subtitle

---
+++ src/components/subtitle.svelte
@@ -0,0 +1,15 @@
+<script>
+  var subtitleSentences = [
+    " peepeepoopoo checker",
+    " failure",
+    " dumbass",
+    " h lover",
+    " h enjoyer",
+    "ly depressed poor fuck",
+    "ly dumb developer",
+  ];
+  var subtitleWord =
+    subtitleSentences[Math.floor(Math.random() * subtitleSentences.length)];
+</script>
+
+<div class="pb-10 text-xl">A professional{subtitleWord}</div>
