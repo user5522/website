8ed45dd151717ad47fd22f58c7ccea0f48a1cc9d
User5522 <oueslatim268@gmail.com>
2022-09-10 17:50:10 +0100

    enhancements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,37 +25,43 @@
 <div class="pt-28 bg-black">
   <div class="flex flex-col items-center justify-center pb-20">
     <div class="flex flex-row items-baseline">
-      <p class="ml-16 text-4xl">User5522</p>
-      <p class="text-xs">/Username5522</p>
+      <div class="text-4xl">User5522</div>
+      <div class="text-xs">/Username5522</div>
     </div>
     <div><Subtitle /></div>
-    <div class="p-10">
-      Welcome to my website, a place where I publish everything I failed at
+    <div class="pl-10 pr-10 items-center flex flex-col justify-center">
+      <div class="mb-7 text-xl">My website is epico, because:</div>
+      <div>
+        - It is made using <a target="_blank" href="https://svelte.dev/"
+          >svelte</a
+        >
+        &
+        <a target="_blank" href="https://kit.svelte.dev"> sveltekit </a>
+      </div>
+      <div>- I made it :{")"}</div>
+      <div>
+        - It's fully opensource on
+        <a
+          target="_blank"
+          href="https://github.com/user5522/user5522.github.io"
+        >
+          my github profile
+        </a> (just like most of my projects)
+      </div>
+      <div>- It's good looking (real)</div>
+      <div>- It'll support more languages soon™</div>
+      <div>- I have nothing else to compliment, but maybe you do</div>
     </div>
-    <div>It's a good website, because:</div>
-    <div>- It is made using svelte</div>
-    <div>- I made it :{")"}</div>
-    <div>
-      - It's fully opensource at
-      <a
-        class="text-blue-400"
-        target="_blank"
-        href="https://github.com/user5522/user5522.github.io"
-      >
-        user5522/user5522.github.io
-      </a> (just like most of my projects)
-    </div>
-    <div>- It's good looking (real)</div>
-    <div>- It'll support more languages soon™</div>
-    <div>- I have nothing else to compliment, but maybe you do</div>
-    <div class="pt-10">Note:</div>
-    <div>
-      I am NOT wanted in several countries for commiting any sort of federal
-      crime.
-    </div>
-    <div class="pb-10">
-      I am an innocent individual, and I definitely didn't commit any of these
-      crimes:
+    <div class="pl-10 pr-10">
+      <div class="pt-10 text-xl">Note:</div>
+      <div>
+        I am NOT wanted in several countries for commiting any sort of federal
+        crime.
+      </div>
+      <div class="pb-10">
+        I am an innocent individual, and I definitely didn't commit any of these
+        crimes:
+      </div>
     </div>
     <div class="flex flex-row gap-4">
       <!-- svelte-ignore a11y-missing-attribute -->
@@ -73,12 +79,13 @@
             View false accusations
           </button>
         </a>
-        <a href="/Accusations"
-          ><button
+        <a href="/Accusations" class="text-white">
+          <button
             class="sm:p-5 p-4 sm:text-base text-sm hover:bg-dark_darker rounded-xl hover:scale-110 active:scale-90 duration-200"
-            >View in a different page</button
-          ></a
-        >
+          >
+            View in a different page
+          </button>
+        </a>
       </Modal>
     </div>
   </div>
