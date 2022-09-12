2b8520b0f301105b4861c7ab87fd4b814e659681
User5522 <oueslatim268@gmail.com>
2022-09-12 11:10:24 +0100

    improve links in /About and make the design more consistent

--- src/routes/[A]bout/+page.svelte
+++ src/routes/[A]bout/+page.svelte
@@ -3,13 +3,7 @@
   import WavesStart from "/src/components/waves_start.svelte";
   import Footer from "/src/components/footer.svelte";
   import { onMount } from "svelte";
-  import Popup from "/src/components/discordTag.svelte";
   import { clickToCopy } from "/src/components/clickToCopy.js";
-  import Modal, { bind } from "svelte-simple-modal";
-  import CloseButton from "/src/components/closeButton.svelte";
-  import { writable } from "svelte/store";
-  const modal = writable(null);
-  const showModal = () => modal.set(bind(Popup));
 
   onMount(() => {
     var start = new Date("11/24/2006 11:50 AM"); //fucking americans man
@@ -40,7 +34,7 @@
 <div class="bg-black pt-28">
   <div class="flex flex-col items-center justify-center pb-20">
     <div class="flex flex-row items-baseline">
-      <p class="ml-16 text-4xl">User5522</p>
+      <p class="text-4xl">User5522</p>
       <p class="text-xs">/Username5522</p>
     </div>
 
@@ -89,45 +83,41 @@
     <div class="flex flex-col items-center gap-5">
       <div class="flex flex-row items-center gap-5">
         <a
-          href="https://www.youtube.com/channel/UCVpdDpi-FiqPg9-pqRJZjHQ"
+          href="https://youtube.com/channel/UCVpdDpi-FiqPg9-pqRJZjHQ"
           target="_blank"
         >
-          <img src="/youtube.svg" alt="youtube link" class="h-10" />
+          <img src="/Youtube.svg" alt="youtube link" class="h-10" />
         </a>
-        <Modal
-          show={$modal}
-          unstyled={false}
-          closeButton={CloseButton}
-          styleContent={{ backgroundColor: "#121212", borderRadius: "0.75rem" }}
-        >
-          <button on:click={showModal}>
-            <img src="/discord.svg" alt="discord username popup" class="h-10" />
-          </button>
-        </Modal>
-
+        <a href="https://discord.com/users/619210431809978368" target="_blank">
+          <img src="Discord.svg" alt="discord link" class="h-10" />
+        </a>
         <a
           href="https://account.xbox.com/en-us/profile?gamertag=Username6736"
           target="_blank"
         >
-          <img src="/xbox.svg" alt="xbox link" class="h-7" />
+          <img src="/Xbox.svg" alt="xbox link" class="h-9" />
         </a>
       </div>
       <div class="flex flex-row items-center gap-5">
         <a href="https://twitter.com/User_5522" target="_blank">
-          <img src="/twitter.svg" alt="twitter link" class="h-8" />
+          <img src="/Twitter.svg" alt="twitter link" class="h-9" />
         </a>
         <a href="https://steamcommunity.com/id/username5522/" target="_blank">
-          <img src="/steam.svg" alt="steam link" class="h-9" />
+          <img src="/Steam.svg" alt="steam link" class="h-10" />
         </a>
         <a href="https://github.com/user5522" target="_blank">
-          <img src="/github.svg" alt="github link" class="h-10" /></a
-        >
+          <img src="/Github.svg" alt="github link" class="h-12" />
+        </a>
       </div>
     </div>
-    <div class="flex flex-row gap-1 pt-5">
-      <div>email me @</div>
-
-      <div use:clickToCopy class="inline-block">oueslatim268@gmail.com</div>
+    <div class="flex flex-row gap-1 pt-5 items-baseline">
+      <div>for any suggestions/reports email me @</div>
+      <div
+        use:clickToCopy
+        class="inline-block bg-dark p-1 duration-200 rounded-lg hover:scale-105 active:scale-95"
+      >
+        oueslatim268@gmail.com
+      </div>
     </div>
   </div>
   <Footer />
