0459145a67bae54e0c48ddddf20bac33be512029
User5522 <oueslatim268@gmail.com>
2022-09-23 19:27:02 +0100

    new build

--- docs/_app/immutable/chunks/1-91b344bc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-2b0eeb63.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-774d43e0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fcf0b3b6.js";export{t as component};
