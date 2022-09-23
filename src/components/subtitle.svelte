da171d8aa3943e41a3fd6fbf36adc0bd36787319
User5522 <oueslatim268@gmail.com>
2022-09-23 19:49:59 +0100

    change le wording

--- src/components/subtitle.svelte
+++ src/components/subtitle.svelte
@@ -1,6 +1,6 @@
 <script>
   var subtitles = [
-    " peepeepoopoo checker",
+    " peepeepoopoo master",
     " failure",
     " dumbass",
     " h lover",
@@ -9,8 +9,7 @@
     "ly dumb developer",
     " rock eater",
   ];
-  var subtitle =
-    subtitles[Math.floor(Math.random() * subtitles.length)];
+  var subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
 </script>
 
 <div class="pb-10 text-xl">A professional{subtitle}.</div>
