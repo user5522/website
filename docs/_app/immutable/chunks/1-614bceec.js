5eae40f0969c750a603027280624c7e8cb910660
Username * <oueslatim268@gmail.com>
2022-10-16 17:37:35 +0100

    new builb

---
+++ docs/_app/immutable/chunks/1-614bceec.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-c5e6d651.js";export{t as component};
--- docs/_app/immutable/chunks/1-c7aae72c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-7f7c27f0.js";export{t as component};
