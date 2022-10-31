33b9d7801a8705a7c34bd37e1c6578092962322a
Username * <oueslatim268@gmail.com>
2022-10-31 18:07:22 +0100

    make the navbar smaller

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -20,33 +20,26 @@
 </script> -->
 
 <div
-  class="fixed top-0 z-50 p-3 lg overflow-auto w-full flex flex-col justify-center align-middle"
+  class="fixed top-0 z-50 p-3.5 lg overflow-auto w-full flex flex-col justify-center align-middle"
 >
   <nav
     id="navbar"
-    class="flex flex-row items-center sm:p-5 p-4 bg-dark rounded-xl  shadow-lg"
+    class="flex flex-row items-center p-4 bg-dark rounded-xl shadow-lg"
   >
     <div
       class="flex flex-row items-center sm:gap-2 gap-1 duration-300"
       id="logo"
     >
-      <!-- <Motion animate={{ rotate: i }} transition={{ duration: 0.5 }} let:motion> 
-      <button on:click={() => (i += 90)}>
-      -->
       <img
         src="/Logo_flat.svg"
         class="items-center sm:h-10 h-9 rounded-full bg-dark_light"
         alt="logo"
       />
-      <!--
-              </button
-        >
-         </Motion> -->
 
       <a href="/" class="text-white">
         <button
           id="logo_text"
-          class="sm:p-3 p-2 sm:text-xl text-base font-semibold duration-200 rounded-lg hover:bg-dark_light hover:scale-110 active:scale-90"
+          class="p-2 sm:text-lg text-base font-semibold duration-200 rounded-lg hover:bg-dark_light hover:scale-110 active:scale-90"
           alt="logo"
         >
           User5522
@@ -56,14 +49,14 @@
     <div class="absolute right-10">
       <a href="/projects" class="text-white">
         <button
-          class="sm:p-3 p-2 hover:scale-110 sm:text-xl text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
+          class="p-2 hover:scale-110 sm:text-lg text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
         >
           Projects
         </button>
       </a>
       <a href="/about" class="text-white">
         <button
-          class="sm:p-3 p-2 hover:scale-110 sm:text-xl text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
+          class="p-2 hover:scale-110 sm:text-lg text-base active:scale-90 duration-200 hover:bg-dark_light rounded-lg"
         >
           About
         </button>
