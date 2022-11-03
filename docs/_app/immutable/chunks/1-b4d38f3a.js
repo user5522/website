c2e0550ed2bda8c661bdd14fc98a93df96754232
Username * <oueslatim268@gmail.com>
2022-11-03 21:07:57 +0100

    new build with latest commits **

--- docs/_app/immutable/chunks/1-b4d38f3a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-7bafb85d.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-ddf57471.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-1a83ffa1.js";export{t as component};
