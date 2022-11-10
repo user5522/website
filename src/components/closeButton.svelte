e7f35fdc67a4e51701740c6045aeb43298d83d31
Username * <oueslatim268@gmail.com>
2022-11-10 18:46:16 +0100

    consistency and aesthetic improvements

--- src/components/closeButton.svelte
+++ src/components/closeButton.svelte
@@ -2,10 +2,10 @@
   export let onClose;
 </script>
 
-<button on:click={onClose} class="absolute top-2 right-10 z-50"
+<button on:click={onClose} class="absolute top-5 right-5 z-50"
   ><img
     src="/icons/close.svg"
     alt="Close popup"
-    class="rounded-full bg-dark_light p-2 duration-200 hover:scale-110 hover:bg-red-900 active:scale-90"
+    class="rounded-full bg-dark_light bg-opacity-60 p-2 backdrop-blur duration-200 hover:scale-110 hover:bg-red-900 active:scale-90"
   /></button
 >
