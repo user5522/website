21e50bcfb6a6b8332df4a9bf0c927ac879aa29f3
Username * <oueslatim268@gmail.com>
2022-11-10 21:27:37 +0100

    rework banners & logos part

--- src/routes/projects/banners/+page.svelte
+++ src/routes/projects/banners/+page.svelte
@@ -13,7 +13,7 @@
   <WavesStart />
 </div>
 <div class="init-div">
-  <div class="flex flex-col items-center justify-center pb-20">
+  <div class="flex flex-col items-center justify-center px-10 pb-20">
     <div class="flex flex-row items-baseline">
       <div class="text-4xl">Banners & Logos</div>
     </div>
@@ -24,39 +24,63 @@
     <div class="flex flex-row items-center gap-1 pt-10 pb-5 text-3xl">
       <a href="https://crbt.app" target="_blank">CRBT</a> Banners
     </div>
-    <div class="flex flex-col gap-5 px-10 text-xl font-semibold">
-      Shapes Banner
-      <img
-        src="/banners/shapes.png"
-        alt="shapes banner"
-        class="min-h-fit rounded-xl"
-      />
-
-      Bubbles Banner
-      <img
-        src="/banners/bubbles.png"
-        alt="bubbles banner"
-        class="min-h-fit rounded-xl"
-      />
-      Color Flow Banner
-      <img
-        src="/banners/colorflow.png"
-        alt="color flow banner"
-        class="min-h-fit  rounded-xl"
-      />
-      Curve Banner - (Remade)
-      <img
-        src="/banners/curve.png"
-        alt="curve banner"
-        class="min-h-fit  rounded-xl"
-      />
-      Wallpaper Banner - (Remade)
-      <img
-        src="/banners/wallpaper.png"
-        alt="wallpaper banner"
-        class="min-h-fit  rounded-xl"
-      />
+    <div class="flex flex-col gap-10">
+      <div class="flex flex-col gap-10 sm:flex-row">
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            Colorflow Banner
+          </div>
+          <img src="/banners/colorflow.png" class="rounded-xl" alt="test" />
+        </div>
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            Bubbles Banner
+          </div>
+          <img src="/banners/bubbles.png" class="rounded-xl" alt="test" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-10 sm:flex-row">
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            Curve Banner [remade]
+          </div>
+          <img src="/banners/curve.png" class="rounded-xl" alt="test" />
+        </div>
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            Shapes Banner
+          </div>
+          <img src="/banners/shapes.png" class="rounded-xl" alt="test" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-10 sm:flex-row">
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            Wallpaper Banner
+          </div>
+          <img src="/banners/wallpaper.png" class="rounded-xl" alt="test" />
+        </div>
+        <div>
+          <div
+            class="flex justify-center pb-2 text-xl font-semibold sm:justify-start"
+          >
+            More banners soon.
+          </div>
+          <img src="/banners/banners.png" class="rounded-xl" alt="test" />
+        </div>
+      </div>
     </div>
+
     <div class="flex items-center pt-10 pb-5 text-3xl">Logos</div>
     <div class="flex flex-col gap-10 sm:flex-row">
       <div class="flex flex-col">
@@ -66,7 +90,7 @@
           User5522 Logo
         </div>
         <div class="rounded-xl bg-dark">
-          <img src="/Logo_flat.svg" class="h-52" alt="Logo" />
+          <img src="/Logo_flat.svg" class="mh-40 w-full sm:h-52" alt="Logo" />
         </div>
       </div>
       <div class="flex flex-col">
@@ -76,7 +100,11 @@
           Vbot Logo
         </div>
         <div class="rounded-xl bg-dark">
-          <img src="/vbot.svg" class="h-52 p-10" alt="Logo" />
+          <img
+            src="/vbot.svg"
+            class="h-40 w-full p-8 sm:h-52 sm:p-10"
+            alt="Logo"
+          />
         </div>
       </div>
     </div>
