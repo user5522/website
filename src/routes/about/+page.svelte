ff9c2bfc9fc8c64c58528e788205e6ad8d159d98
Username * <oueslatim268@gmail.com>
2022-11-12 19:25:19 +0100

    bug fix

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,10 +10,7 @@
       var t = new Date() - new Date(2006, 11, 24);
       t /= 1000 * 60 * 60 * 24 * 365;
       document.getElementById("age").innerHTML =
-        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(3).substr(2);
-
-      document.getElementById("perage").innerHTML =
-        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(2).substr(2);
+        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(3).substring(2);
     }
 
     setInterval(age, 300);
