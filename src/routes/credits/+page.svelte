97347a9965fbc5b22784beff32296c11660bc09f
Username * <oueslatim268@gmail.com>
2022-11-16 11:24:10 +0100

    add a back to homepage button in empty page

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,6 +1,11 @@
 <svelte:head>
   <title>/credits</title>
 </svelte:head>
-<div class="flex flex-col items-center text-xl">
-  Credits page, please come back later.
+<div class="flex flex-col items-center pt-10 text-xl">
+  credits aren't ready for now. Please come back later
+</div>
+<div class="flex justify-center py-10">
+  <a href="/">
+    <button class="blue-button">Back to Homepage</button>
+  </a>
 </div>
