82c728fcac9faad60d481b11b2fe8dc14b569c47
Username * <oueslatim268@gmail.com>
2022-11-24 21:55:50 +0100

    testing css grid

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -1,4 +1,75 @@
 <script>
 </script>
 
-<div>one day you will find random tests here</div>
+<div class="p-10">
+  <div id="cards-container" class="grid gap-5 sm:grid-cols-2">
+    <div
+      class="rounded-xl bg-dark p-5 text-white duration-200 hover:scale-105 active:scale-95"
+    >
+      <div id="post_img">
+        <img src="/placeholder.png" alt="placeholder" />
+      </div>
+      <div
+        id="post_title"
+        class="flex flex-col items-baseline gap-1 py-2 sm:flex-row"
+      >
+        <div class="text-xl font-semibold">This is a much longer title</div>
+      </div>
+      <div id="post_description" class="text-lg">
+        This is a description about this post's content, it is supposed to be a
+        summary for people to know what they will be reading.
+      </div>
+    </div>
+    <div
+      class="rounded-xl bg-dark p-5 text-white duration-200 hover:scale-105 active:scale-95"
+    >
+      <div id="post_img">
+        <img src="/placeholder.png" alt="placeholder" />
+      </div>
+      <div
+        id="post_title"
+        class="flex flex-col items-baseline gap-1 py-2 sm:flex-row"
+      >
+        <div class="text-xl font-semibold">This is a much longer title</div>
+      </div>
+      <div id="post_description" class="text-lg">
+        This is a description about this post's content, it is supposed to be a
+        summary for people to know what they will be reading.
+      </div>
+    </div>
+    <div
+      class="rounded-xl bg-dark p-5 text-white duration-200 hover:scale-105 active:scale-95  "
+    >
+      <div id="post_img">
+        <img src="/placeholder.png" alt="placeholder" />
+      </div>
+      <div
+        id="post_title"
+        class="flex flex-col items-baseline gap-1 py-2 sm:flex-row"
+      >
+        <div class="text-xl font-semibold">This is a much longer title</div>
+      </div>
+      <div id="post_description" class="text-lg">
+        This is a description about this post's content, it is supposed to be a
+        summary for people to know what they will be reading.
+      </div>
+    </div>
+    <div
+      class="rounded-xl bg-dark p-5 text-white duration-200 hover:scale-105 active:scale-95  "
+    >
+      <div id="post_img">
+        <img src="/placeholder.png" alt="placeholder" />
+      </div>
+      <div
+        id="post_title"
+        class="flex flex-col items-baseline gap-1 py-2 sm:flex-row"
+      >
+        <div class="text-xl font-semibold">This is a much longer title</div>
+      </div>
+      <div id="post_description" class="text-lg">
+        This is a description about this post's content, it is supposed to be a
+        summary for people to know what they will be reading.
+      </div>
+    </div>
+  </div>
+</div>
