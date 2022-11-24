777bfd6cea84463c0880d50c8c74289300f50f41
Username * <oueslatim268@gmail.com>
2022-11-24 14:51:15 +0100

    fix age counter

--- src/lib/ageCounter.svelte
+++ src/lib/ageCounter.svelte
@@ -2,12 +2,11 @@
   import { onMount } from "svelte";
   onMount(() => {
     function age() {
-      var t = new Date() - new Date(2006, 11, 24);
+      var t = new Date() - new Date("2006-11-24");
       t /= 1000 * 60 * 60 * 24 * 365;
       document.getElementById("age").innerHTML =
-        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(3).substring(2);
+        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(2).substring(2);
     }
-
     setInterval(age, 300);
   });
 </script>
