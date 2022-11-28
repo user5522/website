f43f0873742a391a696cfbbff6c4cdf460860416
Username * <oueslatim268@gmail.com>
2022-11-28 19:35:35 +0100

    easter egg :)

--- src/lib/emojis/wave-emoji.svelte
+++ src/lib/emojis/wave-emoji.svelte
@@ -1,5 +1,9 @@
-<img
-  src="/twemojis/wavinghand.svg"
-  alt="waving hand sign emote"
-  class="inline-block h-6 align-top"
-/>
+<div
+  class="inline-block duration-200 hover:scale-102 active:rotate-180 active:scale-95"
+>
+  <img
+    src="/twemojis/wavinghand.svg"
+    alt="waving hand sign emote"
+    class="h-6 align-top"
+  />
+</div>
