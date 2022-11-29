242e4ba7bcc002289a39b65f0c48a4d0e92b1ac7
Username * <oueslatim268@gmail.com>
2022-11-29 13:16:08 +0100

    remake credits page

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -2,16 +2,10 @@
   import Navbar from "$lib/navbar.svelte";
   import WavesStart from "$lib/waves_start.svelte";
   import Footer from "$lib/footer.svelte";
+  import Heart from "$lib/emojis/heart.svelte";
+  import Flushed from "$lib/emojis/flushed-emoji.svelte";
+  import { tooltip } from "$lib/tooltip.js";
 </script>
-
-<!-- <div class="flex flex-col items-center pt-10 text-xl">
-  credits aren't ready for now. Please come back later
-</div>
-<div class="flex justify-center py-10">
-  <a href="/">
-    <button class="blue-button">Back to Homepage</button>
-  </a>
-</div> -->
 
 <svelte:head>
   <title>/credits</title>
@@ -22,37 +16,68 @@
   <WavesStart />
 </div>
 <div class="init-div px-5">
-  <div class="flex flex-col items-center justify-center gap-5">
+  <div class="flex flex-col items-center justify-center">
     <div class="text-4xl">Credits</div>
-    <div class="text-xl">A place to say thank you</div>
+    <div class="text-xl">A place to say thank you <Heart /></div>
   </div>
-  <div class="mx-auto max-w-6xl gap-16 text-xl">
-    <div class="text-2xl">I want to thank:</div>
+  <div class="flex flex-col items-center justify-center gap-4 text-xl">
     <div>
-      - <strong
-        >All of my friends, family & everyone who had an influence on me (in a
-        good way)</strong
-      >
+      This website wouldn't've been as good as it is without these awesome
+      people:
     </div>
-    <div>
-      - <strong>The people behind Sveltekit</strong>, for making a great, fast &
-      reliable javascript framework.
+    <div class="flex flex-col gap-5">
+      <div class="pl-8 sm:pl-12">
+        <div
+          use:tooltip={{ text: "For having a good influence on me" }}
+          class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+        >
+          <div>My family</div>
+          <div>& friends</div>
+        </div>
+      </div>
+      <div class="pl-56 sm:pl-108">
+        <div
+          use:tooltip={{ text: "For creating one of the best frameworks" }}
+          class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+        >
+          <div>SvelteKit</div>
+          <div>Devs</div>
+        </div>
+      </div>
+      <div class="pl-16 sm:pl-32">
+        <div
+          use:tooltip={{
+            text: "For the placeholder image & all the tips",
+          }}
+          class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+        >
+          <div>Clembs</div>
+        </div>
+      </div>
+      <div class="pl-2 sm:pl-80">
+        <div
+          use:tooltip={{
+            text: "For their huge free collection of icons",
+          }}
+          class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+        >
+          <div>FontAwesome</div>
+        </div>
+      </div>
+      <div class="self-center">
+        <div
+          use:tooltip={{ text: "For visiting my website" }}
+          class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+        >
+          <div>You <Flushed /></div>
+        </div>
+      </div>
     </div>
-    <div>
-      - <a href="https://clembs.com" target="_blank" rel="noreferrer"
-        ><strong>Clembs</strong></a
-      >, idk I'm pretty sure he inspired me & told me about SvelteKit
-    </div>
-    <div>
-      - <strong><a href="/">Myself</a></strong>, because damn brother, this took
-      forever to make.
-    </div>
-    <div>
-      - All the contributors: <strong>me</strong>, <strong>myself</strong> &
-      <strong>I</strong>.
-    </div>
-    <div>
-      - <strong>FontAwesome</strong>, for their huge collection of free icons
+  </div>
+
+  <div class="flex justify-center">
+    <div class="text-xl">
+      Made with <Heart /> by User5522.
     </div>
   </div>
 </div>
