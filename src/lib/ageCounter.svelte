8a5d58ac4642563cec41e4bc679b7098467bb346
Username * <oueslatim268@gmail.com>
2022-12-16 09:45:21 +0100

    make agecounter execute once, to avoid spammed errors in console

--- src/lib/ageCounter.svelte
+++ src/lib/ageCounter.svelte
@@ -1,14 +1,9 @@
 <script>
-  import { onMount } from "svelte";
-  onMount(() => {
-    function age() {
-      var t = new Date() - new Date("2006-11-24");
-      t /= 1000 * 60 * 60 * 24 * 365;
-      document.getElementById("age").innerHTML =
-        "<span>" + Math.floor(t) + "</span>." + (t % 1).toFixed(2).substring(2);
-    }
-    setInterval(age, 300);
-  });
+  var t = new Date() - new Date("2006-11-24");
+  t /= 1000 * 60 * 60 * 24 * 365;
+  let age = Math.floor(t) + "." + (t % 1).toFixed(2).substring(2);
 </script>
 
-<div id="age" class="inline-block" />
+<div id="age" class="inline-block">
+  {age}
+</div>
