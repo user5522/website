fb5f9621a12d44be6404f7a491f360d9b031fb26
Username * <oueslatim268@gmail.com>
2022-12-21 14:11:04 +0100

    start using IntersectionObserver instead of DOM

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -9,44 +9,56 @@
 
 		if (clicks >= 4) {
 			spinning = true;
-			clicks = 0;
-			setTimeout(() => {
+			const timeout = setTimeout(() => {
+				clicks = 0;
 				spinning = false;
-			}, 10000);
+				clearTimeout(timeout);
+			}, 1000);
 		}
 	}
 
-	onMount(() => {
-		window.onscroll = function () {
-			scrollFunction();
-		};
+	let observedSpan;
+	let isAnimated = false;
 
-		function scrollFunction() {
-			if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-				document.getElementById('padding').style.padding = '0px';
-				document.getElementById('navbar').style.borderRadius = '0px';
-				document.getElementById('navbar').classList.add('sm:bg-opacity-50');
-				document.getElementById('navbar').classList.add('sm:backdrop-blur-md');
-				document.getElementById('padding').style.transitionDuration = '400ms';
-				document.getElementById('navbar').style.transitionDuration = '400ms';
-			} else {
-				document.getElementById('padding').style.padding = '12px';
-				document.getElementById('navbar').style.borderRadius = '12px';
-				document.getElementById('navbar').classList.remove('sm:bg-opacity-50');
-				document.getElementById('navbar').classList.remove('sm:backdrop-blur-md');
-				document.getElementById('padding').style.transitionDuration = '400ms';
-				document.getElementById('navbar').style.transitionDuration = '400ms';
-			}
-		}
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						isAnimated = true;
+					}
+					if (!entry.isIntersecting) {
+						isAnimated = false;
+					}
+				});
+			},
+			{ root: null }
+		);
+		console.log(observedSpan);
+		observer.observe(observedSpan);
+		return () => observer.unobserve(observedSpan);
 	});
+
+	const navItems = [
+		{ name: 'Projects', href: 'projects' },
+		{ name: 'Collections', href: 'projects' },
+		{ name: 'About', href: 'projects' }
+	];
 </script>
 
 <div class="pb-28">
 	<div
 		id="padding"
-		class="fixed top-0 z-50 flex w-full flex-col justify-center overflow-auto p-3 align-middle"
+		class="duration-400 transition-all fixed top-0 z-50 flex w-full flex-col justify-center overflow-auto align-middle {isAnimated
+			? 'p-3'
+			: ''}"
 	>
-		<div id="navbar" class="flex flex-row rounded-xl bg-dark p-4 shadow-lg">
+		<div
+			id="navbar"
+			class="flex flex-row duration-400 transition-all bg-dark p-4 shadow-lg {isAnimated
+				? 'rounded-xl'
+				: 'sm:bg-opacity-50 sm:backdrop-blur-md'}"
+		>
 			<div class="flex w-1/2 flex-row items-center gap-1 duration-300 sm:gap-2" id="branding">
 				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<div id="logo" class="duration-200 hover:scale-105 active:scale-95" on:click={handleClick}>
@@ -69,31 +81,20 @@
 				</a>
 			</div>
 			<div class="flex w-1/2 justify-end">
-				<a href="/projects" class="text-white">
-					<button
-						class="rounded-lg p-2 text-base font-medium duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
-					>
-						Projects
-					</button>
-				</a>
-				<a href="/collections" class="text-white">
-					<button
-						class="rounded-lg p-2 text-base font-medium duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
-					>
-						Collections
-					</button>
-				</a>
-				<a href="/about" class="text-white">
-					<button
-						class="rounded-lg p-2 text-base font-medium duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
-					>
-						About
-					</button>
-				</a>
+				{#each navItems as navItem}
+					<a href={navItem.href} class="text-white">
+						<button
+							class="rounded-lg p-2 text-base font-medium duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
+						>
+							{navItem.name}
+						</button>
+					</a>
+				{/each}
 			</div>
 		</div>
 	</div>
 </div>
+<span bind:this={observedSpan} />
 
 <style>
 	@keyframes spin {
