aa4bf087880935dae06ec23cf8ca7ef865a27a40
Username * <oueslatim268@gmail.com>
2022-12-27 00:01:26 +0100

    better colors, with better contrast | change default selection highlight colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,8 @@ module.exports = {
 				dark_darker: '#101010',
 				true_gray: '#191919',
 				light_true_gray: '#2f2f2f',
-				lighter_true_gray: '#5f5f5f'
+				lighter_true_gray: '#5f5f5f',
+				dark_blue: '#051531'
 			},
 			rotate: {
 				158: '158deg',
