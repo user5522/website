89b6103051d5b7a124f2f4c570808e2fa24aee18
Username * <oueslatim268@gmail.com>
2022-12-27 23:23:10 +0100

    new vbot projects page, still WIP

--- src/routes/projects/vbot/+page.svelte
+++ src/routes/projects/vbot/+page.svelte
@@ -13,42 +13,49 @@
 		</div>
 	</div>
 	<div class="flex flex-col gap-10">
-		<div class="flex flex-col gap-10 sm:flex-row">
-			<div class="flex justify-center sm:w-1/3">
-				<div class="rounded-xl bg-dark">
-					<img src="/vbot/vbotbranding.png" alt="vbot branding" class="h-56 p-10" />
+		<div class="flex flex-col gap-10 sm:flex-row justify-center">
+			<div class="flex justify-center">
+				<div class="rounded-xl bg-dark hover:scale-102 active:scale-95 duration-200">
+					<img src="/vbot/vbotbranding.png" alt="vbot branding" class=" h-56 p-10" />
 				</div>
 			</div>
-			<div class="sm:w-1/3">
-				<div class="flex flex-row justify-center text-xl font-semibold">
-					Now rewritten in Discord.JS
+			<div class="sm:w-2/4">
+				<div class="flex flex-row text-xl font-semibold">
+					Reimagined and Rebuilt with Discord.JS
 				</div>
 				<div class="text-lg">
-					Vbot was rewritten from the ground up using <a
-						href="https://discord.js.org/#/"
+					Vbot has undergone a total overhaul with <a
+						href="https://discord.js.org/"
 						target="_blank"
-						rel="noreferrer">Discord.JS</a
-					> to have the latest and greatest features and yeah speed vroooom efficiency yeeeeeeee, if
-					you haven't noticed yet this is still a WIP and i want to see what a long ass marketting paragraph
-					would look like. so far so good, right?
+						rel="noreferrer"
+					>
+						Discord.JS</a
+					>, giving it lightning-fast speed and all the coolest features. But don't just take my
+					word for it, it's still a work in progress, so bear with me as I make Vbot the ultimate
+					Discord companion. In the meantime, let's see how far I can stretch this marketing spiel,
+					shall we?
 				</div>
 			</div>
 		</div>
-		<div class="flex flex-col items-center justify-center gap-5 sm:flex-row">
-			<div class="sm:w-2/3 sm:pl-32">
-				<div class="flex flex-row justify-center text-xl font-semibold sm:justify-start">
-					Blablabla more marketting crap
+		<div class="flex flex-col gap-10 sm:flex-row justify-center">
+			<div class="sm:w-2/4">
+				<div class="flex flex-row text-xl font-semibold">
+					Get ready for some seriously impressive marketing speak!
 				</div>
 				<div class="text-lg">
-					insert cool thing that isn't available, i literally don't care if you like this or not,
-					you can give feedback using <a href="/about#Links">one of these</a>, invite V
-					<div class="inline-block text-sm">(ery good ro)</div>
-					bot to benefit absolutely nothing.
+					Vbot has all the bells and whistles you could ever want, including a super secret feature
+					that's not even available yet (but trust me, it's going to blow your mind). I don't care
+					if you like it or not, but if you want to give me feedback, go ahead and invite Vbot to
+					your server. It won't give you any tangible benefits for now, but hey, it never hurts to
+					have a cool bot around, right?
 				</div>
 			</div>
-			<div class="flex justify-center sm:w-1/3 sm:pr-32">
+			<div class="flex justify-center self-center gap-5">
 				<a href="/vbot" target="blank">
 					<button class="blue-button text-lg">Invite Vbot</button>
+				</a>
+				<a href="/vbot/source" target="blank">
+					<button class="hidden-button text-white text-lg">Source code</button>
 				</a>
 			</div>
 		</div>
