01fc8da38c149f9659a94a820d6d8a84ba7f6c15
Username * <oueslatim268@gmail.com>
2023-01-02 21:49:39 +0100

    yeah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,91 +74,6 @@
 <svelte:head>
 	<title>Welcome - user5522.tk</title>
 </svelte:head>
-<!-- 
-<div class="init-div px-5">
-	<div class="flex flex-col items-center justify-center gap-5">
-		<div class="flex justify-center flex-col items-center">
-			<div class="flex flex-row items-baseline">
-				<div class="text-4xl">User5522</div>
-				<div class="text-base">/Username5522</div>
-			</div>
-			<Subtitle />
-		</div>
-		<div class="flex flex-col items-center gap-5 justify-center">
-			<div class="overflow-hidden whitespace-nowrap text-2xl">
-				Welcome to <strong>user5522.tk</strong>!
-			</div>
-			<div class="text-xl">
-				Hey, I am User5522, I like calling myself a designer, a developer & a problems-solver (sort
-				of)
-			</div>
-		</div>
-		<div class="text-lg">I have a few projects, that include:</div>
-
-		<div class=" grid gap-5 sm:grid-cols-2 sm:max-w-5xl duration-200 2xl:max-w-7xl">
-			{#each cardItems as cardItem}
-				<a
-					class="w-full rounded-2xl bg-dark text-white duration-200 hover:scale-102 active:scale-95"
-					href={cardItem.href}
-				>
-					<div>
-						<div class="h-full">
-							<div class="sm:h-2/3">
-								<div class="flex w-full items-center justify-center">
-									<img src={cardItem.imgURL} alt={cardItem.imgAlt} class="rounded-t-xl" />
-								</div>
-							</div>
-							<div class="p-5 sm:h-1/3">
-								<div class="flex flex-row items-baseline">
-									<div id="project_title" class=" text-xl font-bold">{cardItem.title}</div>
-								</div>
-								<div>
-									<div id="project_description" class="text-lg">
-										{cardItem.description}
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</a>
-			{/each}
-		</div>
-
-		<div class="rounded-xl bg-dark p-2 text-lg duration-200 hover:scale-102 active:scale-95">
-			Additional projects can be viewed, <a href="/projects">right here.</a>
-		</div>
-
-		<div class="rounded-xl bg-dark p-2 text-lg duration-200 hover:scale-102 active:scale-95">
-			Find out more about me, <a href="/about">here.</a>
-		</div>
-
-		<div>
-			<strong class="text-xl">Unimportant note:</strong>
-			<div>
-				Despite what you may have heard, I am not a wanted criminal in multiple countries.<br />
-				In fact, I am a law-abiding citizen who has never committed any federal offenses.
-			</div>
-		</div>
-		<div class="flex flex-row gap-5">
-			svelte-ignore a11y-missing-attribute
-			<Modal
-				show={$modal}
-				unstyled={false}
-				closeButton={CloseButton}
-				styleWindow={{
-					borderRadius: '.8rem'
-				}}
-				classContent="rounded-xl bg-dark"
-				><a>
-					<button class="blue-button" on:click={showModal}>False accusations </button>
-				</a>
-				<a href="/accusations" class="text-white">
-					<button class="hidden-button">View page</button>
-				</a>
-			</Modal>
-		</div>
-	</div>
-</div> -->
 
 <div class="init-div px-5">
 	<div class="mx-auto flex max-w-4xl flex-col gap-7 justify-center items-center">
@@ -282,6 +197,7 @@
 			</div>
 		</div>
 		<div class="flex flex-row gap-5">
+			<!-- svelte-ignore a11y-missing-attribute -->
 			<Modal
 				show={$modal}
 				unstyled={false}
