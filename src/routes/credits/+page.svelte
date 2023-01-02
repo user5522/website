e7916c226b615810ae3c24708f5792ca5b3ee465
Username * <oueslatim268@gmail.com>
2023-01-02 21:48:40 +0100

    use tippy here + minor improvements

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import Heart from '$lib/twemojis/heart.svelte';
 	import Flushed from '$lib/twemojis/flushed.svelte';
-	import { tooltip } from '$lib/tooltip.js';
+	import tippy from 'svelte-tippy';
+
+	const getRandomX = () => {
+		return `${Math.random() * 100}%`;
+	};
 </script>
 
 <svelte:head>
@@ -12,65 +16,123 @@
 	<div class="flex flex-col items-center justify-center">
 		<div class="text-4xl">Credits</div>
 		<div class="text-xl">
-			A place to say thank you <Heart />
+			A place to say thanks <Heart />
 		</div>
 	</div>
 	<div class="flex flex-col items-center justify-center gap-4 text-xl">
-		<div>This website wouldn't've been as good as it is without these awesome people:</div>
-		<div class="flex flex-col gap-5">
-			<div class="pl-8 sm:pl-12">
+		<div>
+			Thanks for visiting <strong>user5522.tk</strong>, I really appreciate it (probably)
+		</div>
+		<div
+			class="h-full flex flex-col rounded-xl bg-dark duration-200 hover:scale-102 active:scale-95 sm:backdrop-blur-sm p-5 gap-5"
+		>
+			<div class="text-xl text-center">You might enjoy this website, because:</div>
+			<ul>
+				<li>
+					- It is built with <a target="_blank" href="https://kit.svelte.dev" rel="noreferrer"
+						>sveltekit</a
+					>
+				</li>
+				<li>- It's created by me {':)'}</li>
+				<li>
+					- it's open source on
+					<a target="github" href="https://github.com/user5522/website" rel="noreferrer">
+						GitHub
+					</a> (just like most of my projects btw)
+				</li>
+				<li>- It's visually appealing in my opinion</li>
+				<li>- It'll support more languages soon™ (maybe)</li>
+				<li>
+					- I have nothing else to compliment, but <a href="/about#Links">maybe you do</a>.
+				</li>
+			</ul>
+		</div>
+		<div>
+			<strong>user5522.tk</strong> would not be as good as it is without the help of these amazing people:
+		</div>
+		<div class="flex flex-col gap-5 p-20">
+			<div style="margin-left: {getRandomX()}">
 				<div
-					use:tooltip={{ text: 'For having a good influence on me' }}
-					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+					use:tippy={{
+						content: 'for having a good influence on me',
+						arrow: false,
+						animation: 'shift-away',
+						interactive: true,
+						theme: 'dark',
+						placement: 'bottom',
+						duration: 0,
+						offset: [0, 5]
+					}}
+					class="flex whitespace-nowrap w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
 				>
-					<div>My family</div>
-					<div>& friends</div>
+					<li>My family</li>
+					<li>& friends</li>
 				</div>
 			</div>
-			<div class="pl-28 sm:pl-108">
+			<div style="margin-left: {getRandomX()}">
 				<div
-					use:tooltip={{
-						text: 'For creating a great framework'
+					use:tippy={{
+						content: 'for creating an awesome framework',
+						arrow: false,
+						animation: 'shift-away',
+						interactive: true,
+						theme: 'dark',
+						placement: 'bottom',
+						duration: 0,
+						offset: [0, 5]
 					}}
 					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
 				>
-					<div>SvelteKit</div>
+					<div>Svelte</div>
 					<div>Devs</div>
 				</div>
 			</div>
-			<div class="pl-16 sm:pl-32">
+			<div style="margin-left: {getRandomX()}">
 				<div
-					use:tooltip={{
-						text: 'For the placeholder image & all the tips'
-					}}
-					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
-				>
-					<div>Clembs</div>
-				</div>
-			</div>
-			<div class="pl-2 sm:pl-80">
-				<div
-					use:tooltip={{
-						text: 'For their huge free collection of icons'
-					}}
-					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
-				>
-					<div>FontAwesome</div>
-				</div>
-			</div>
-			<div class="pl-40 sm:pl-36">
-				<div
-					use:tooltip={{
-						text: 'For their free hosting'
+					use:tippy={{
+						content: 'for providing free hosting',
+						arrow: false,
+						animation: 'shift-away',
+						interactive: true,
+						theme: 'dark',
+						placement: 'bottom',
+						duration: 0,
+						offset: [0, 5]
 					}}
 					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
 				>
 					<div>Vercel</div>
 				</div>
 			</div>
+			<div style="margin-left: {getRandomX()}">
+				<div
+					use:tippy={{
+						content: 'for the placeholder img and all the tips',
+						arrow: false,
+						animation: 'shift-away',
+						interactive: true,
+						theme: 'dark',
+						placement: 'bottom',
+						duration: 0,
+						offset: [0, 5]
+					}}
+					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
+				>
+					<div>Clembs</div>
+				</div>
+			</div>
 			<div class="self-center">
 				<div
-					use:tooltip={{ text: 'For visiting my website' }}
+					use:tippy={{
+						content: 'for visiting my website',
+						arrow: false,
+						animation: 'shift-away',
+						interactive: true,
+						theme: 'dark',
+						placement: 'bottom',
+						duration: 0,
+						offset: [0, 5]
+					}}
 					class="flex w-fit flex-col items-center justify-center rounded-full bg-dark p-4 duration-200 hover:scale-102 active:scale-95"
 				>
 					<div>You <Flushed /></div>
@@ -79,7 +141,7 @@
 		</div>
 	</div>
 
-	<div class="flex justify-center">
+	<div class="flex  justify-center">
 		<div class="text-xl">
 			Made with <Heart /> by User5522.
 		</div>
