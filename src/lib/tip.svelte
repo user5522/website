0312caaa33a1fac5bb47347c5c8bc74ba0938852
Username * <oueslatim@pm.me>
2023-04-12 12:04:59 -0800

    an other bug fix nice yeah

--- src/lib/tip.svelte
+++ src/lib/tip.svelte
@@ -25,8 +25,8 @@
 	export let linkText;
 </script>
 
-<div class="py-5">
-	<div class="rounded-xl bg-dark text-white p-2 hover-anim" class:hidden={$isHidden}>
+<div class="py-5" class:hidden={$isHidden}>
+	<div class="rounded-xl bg-dark text-white p-2 hover-anim">
 		{text} <a {href}>{linkText}</a>
 		<button
 			class="items-center rounded-lg duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 px-2 py-1 text-xl"
