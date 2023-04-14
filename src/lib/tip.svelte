431d3188da1bcc33c2a54b5c800f4d9266d39019
Username * <oueslatim@pm.me>
2023-04-14 13:13:27 -0700

    bug fix

--- src/lib/tip.svelte
+++ src/lib/tip.svelte
@@ -30,7 +30,7 @@
 </script>
 
 <div class="py-5" class:hidden={$isHidden}>
-	<div class="rounded-xl bg-dark text-white p-2 hover-anim">
+	<div class="rounded-xl bg-dark text-white p-2 whitespace-nowrap hover-anim">
 		{text} <a {href}>{linkText}</a>
 		<button
 			class="items-center rounded-lg duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 px-2 py-1 text-xl"
