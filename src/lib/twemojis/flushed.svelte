69bc6cb1bcf4b77c320c83e7e0e476e120218908
Username * <oueslatim@pm.me>
2023-04-15 14:06:03 -0700

    remove useless emojis & make the rest hoverable/clicky

--- src/lib/twemojis/flushed.svelte
+++ src/lib/twemojis/flushed.svelte
@@ -1,1 +1,5 @@
-<img src="/twemojis/flushed.svg" alt="flushed twitter emoji" class="inline-block h-7 w-7" />
+<img
+	src="/twemojis/flushed.svg"
+	alt="flushed face twitter emoji"
+	class="inline-block h-7 w-7 duration-200 hover:scale-105 active:scale-95"
+/>
--- src/lib/twemojis/heart.svelte
+++ src/lib/twemojis/heart.svelte
@@ -1,1 +1,9 @@
-<img src="/twemojis/heart.svg" alt="red heart twitter emoji" class="inline-block h-7 w-7" />
+<script>
+	export let classes;
+</script>
+
+<img
+	src="/twemojis/heart.svg"
+	alt="red heart twitter emoji"
+	class="inline-block h-7 w-7 duration-200 hover:scale-105 active:scale-95 {classes}"
+/>
