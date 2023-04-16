f7172e02375b42a0fe9322d3488b065f8c399f73
Username * <oueslatim@pm.me>
2023-04-16 07:20:11 -0700

    feat: add top level await vite plugin to supress errors at build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		topLevelAwait({
+			promiseExportName: '__tla',
+			promiseImportName: (i) => `__tla_${i}`
+		}),
+		sveltekit()
+	]
 };
 
 export default config;
