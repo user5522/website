53f4b5a33dec7799865256e9fa3871eb5ab208e0
Username * <oueslatim@pm.me>
2023-06-04 15:25:37 +0100

    light theme bug fixes

--- src/lib/components/tip.svelte
+++ src/lib/components/tip.svelte
@@ -31,7 +31,9 @@
 
 <div class="py-5" class:hidden={$isHidden}>
 	<div class="flex flex-row gap-2 bg-dark text-white rounded-xl p-2 hover-anim">
-		<div class="self-center">{text} <a {href}>{linkText}</a></div>
+		<div class="self-center">
+			{text} <a {href} class="text-blue-300">{linkText}</a>
+		</div>
 		<div class="self-center">
 			<button
 				class="hover:bg-dark_light px-2 py-1 rounded-lg text-xl hover-anim"
