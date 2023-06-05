a5e741fc4e0482e23788b758706ec242dc52862c
Username * <oueslatim@pm.me>
2023-06-05 10:00:17 +0100

    new easter egg in the new name component that will be replaced in /

---
+++ src/lib/utilities/name.svelte
@@ -0,0 +1,34 @@
+<script>
+	let letters = [
+		{
+			letter: 'U'
+		},
+		{
+			letter: 's'
+		},
+		{
+			letter: 'e'
+		},
+		{
+			letter: 'r'
+		},
+		{
+			letter: '5'
+		},
+		{
+			letter: '5'
+		},
+		{
+			letter: '2'
+		},
+		{
+			letter: '2'
+		}
+	];
+</script>
+
+<div class="text-4xl font-semibold flex flex-row">
+	{#each letters as letters}
+		<div class="active:pt-4 duration-200 rounded-xl hover:cursor-pointer">{letters.letter}</div>
+	{/each}
+</div>
