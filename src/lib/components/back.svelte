681c0cbfdb8a711fe6b6c0d07cf71165d488d283
Username * <oueslatim@pm.me>
2023-06-05 09:58:06 +0100

    make the "<- back" button a component

---
+++ src/lib/components/back.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let label = 'Back';
+</script>
+
+<div class="hover-anim flex flex-row gap-3 rounded-xl bg-dark py-2 px-3">
+	<img src="/icons/arrow_left.svg" alt="left arrow" />
+	<div class="text-xl font-medium text-white">{label}</div>
+</div>
