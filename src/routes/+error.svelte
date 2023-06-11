fff2ff958894db6876ee7117b9366592e513339f
Username * <oueslatim@pm.me>
2023-06-11 16:16:17 +0100

    much faster and better error page + bug fixes

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,43 +1,38 @@
 <script>
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		document.getElementById('pathname').innerHTML = window.location.pathname;
-	});
 </script>
 
 <svelte:head>
 	<title>{$page.error.message}</title>
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center gap-5 p-5 ">
+<div class="flex flex-col items-center justify-center gap-5 p-5">
 	<div class="flex flex-col items-center justify-center gap-1">
 		<div class="text-4xl font-semibold">
 			{$page.status}: {$page.error.message}
 		</div>
-		<div class="text-3xl" id="pathname" />
+		<div class="text-3xl">{$page.url.pathname}</div>
 	</div>
 	<div>
 		<div class="text-2xl">This looks like an error.. & that's unfortunate.</div>
 		<div class="flex flex-col gap-5">
 			<div class="flex justify-center text-2xl">Maybe try one of these solutions:</div>
-			<div
-				class="rounded-xl bg-dark text-white p-3 text-lg duration-200 hover:scale-102 active:scale-95"
+			<ul
+				class="list-disc list-inside rounded-xl bg-dark text-white p-3 text-lg duration-200 hover:scale-102 active:scale-95"
 			>
-				<li>- Checking if the URL/Link you received exists</li>
-				<li>- Taking a quick sip of water</li>
-				<li>- Looking over for any typos in the URL</li>
+				<li>Checking if the URL/Link you received exists</li>
+				<li>Taking a quick sip of water</li>
+				<li>Looking over for any typos in the URL</li>
 				<li>
-					- <a
+					<a
 						href="https://google.com/search?q=have+you+tried+turning+it+off+and+on+again"
 						class="text-blue-300"
 						target="_blank"
 						rel="noreferrer">Turning it off & on again?</a
 					>
 				</li>
-				<li>- Waiting till this gets fixed</li>
-			</div>
+				<li>Waiting till this gets fixed</li>
+			</ul>
 			<div class="flex justify-center text-xl">If none of those solutions worked:</div>
 		</div>
 	</div>
@@ -51,7 +46,7 @@
 	</div>
 </div>
 <div
-	class="absolute top-0 right-0 -z-20 rotate-180 text-8xl font-semibold dark:text-white text-black opacity-50 dark:opacity-30 sm:text-9xl"
+	class="absolute top-0 right-0 select-none rotate-180 text-8xl font-semibold dark:text-white text-black opacity-50 dark:opacity-30 sm:text-9xl"
 >
 	Damn.
 </div>
