622d128645f0c19a30592ae15287cf4f78b06742
Username * <oueslatim@pm.me>
2023-06-23 16:02:45 +0100

    some background blurring in the back component

--- src/lib/components/back.svelte
+++ src/lib/components/back.svelte
@@ -2,7 +2,9 @@
 	export let label = 'Back';
 </script>
 
-<div class="hover-anim flex flex-row gap-3 rounded-xl bg-dark py-2 px-3">
-	<img src="/icons/arrow_left.svg" alt="left arrow" loading="lazy" />
+<div
+	class="hover-anim flex flex-row gap-2 items-center rounded-xl bg-dark md:bg-opacity-50 md:backdrop-blur-lg py-2 px-3"
+>
+	<img src="/icons/arrow_left.svg" class="h-5" alt="left arrow" loading="lazy" />
 	<div class="text-xl font-medium text-white">{label}</div>
 </div>
