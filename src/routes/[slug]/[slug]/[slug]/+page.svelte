214de63f17df0e5f465e4acd42af5d8f40aaecbd
Username * <oueslatim@pm.me>
2023-07-08 13:54:34 +0100

    feat: show the item instead of redirecting to it

--- src/routes/[slug]/[slug]/[slug]/+page.svelte
+++ src/routes/[slug]/[slug]/[slug]/+page.svelte
@@ -5,19 +5,34 @@
 	import { page } from '$app/stores';
 
 	let filePath;
+	const supportedImageExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+	const supportedVideoExtensions = ['.mp4', '.mov', '.webm'];
 
 	const unsubscribe = page.subscribe((value) => {
 		filePath = value.url.pathname;
 	});
+
+	let fileExtension = '';
+	let fileTag = '';
+	let downloadURL = '';
 
 	onMount(async () => {
 		if (filePath) {
 			try {
 				const fileRef = ref(storage, filePath);
 
-				const downloadURL = await getDownloadURL(fileRef);
+				downloadURL = await getDownloadURL(fileRef);
+				fileExtension = getFileExtension(filePath);
 
-				window.location.href = downloadURL;
+				if (supportedImageExtensions.includes(fileExtension)) {
+					fileTag = 'img';
+				} else if (supportedVideoExtensions.includes(fileExtension)) {
+					fileTag = 'video';
+				} else {
+					fileTag = 'script';
+				}
+
+				// window.location.href = downloadURL;
 			} catch (error) {
 				console.error('Error fetching file:', error);
 			}
@@ -27,4 +42,17 @@
 	onDestroy(() => {
 		unsubscribe();
 	});
+
+	function getFileExtension(filename) {
+		const extensionIndex = filename.lastIndexOf('.');
+		return extensionIndex === -1 ? '' : filename.slice(extensionIndex);
+	}
 </script>
+
+{#if fileTag === 'img'}
+	<img src={downloadURL} />
+{:else if fileTag === 'video'}
+	<video src={downloadURL} controls />
+{:else}
+	<script src={downloadURL}></script>
+{/if}
