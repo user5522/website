2eb2009419e1fb1bf75384af6811f54bdccbf874
Username * <oueslatim@pm.me>
2023-07-10 17:05:25 +0100

    semi-rewrite the navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
-	import { clickOutside } from '$lib/utilities/clickOutside.js';
+	import { clickOutside } from '$lib/utilities/clickOutside';
 	import Theme from '$lib/theme/theme.svelte';
 
 	let clicks = 0;
@@ -41,11 +41,9 @@
 	});
 
 	const navItems = [
-		// isHide is for the mobile hamburger menu
-		{ name: 'Projects', href: '../projects', isHide: true, isPCHide: false },
-		{ name: 'Collections', href: '../collections', isHide: true, isPCHide: false },
-		{ name: 'About', href: '../about', isHide: true, isPCHide: false },
-		{ name: 'Credits', href: '../credits', isHide: true, isPCHide: true }
+		{ name: 'Projects', href: '../projects', isHide: true },
+		{ name: 'Collections', href: '../collections', isHide: true },
+		{ name: 'Credits', href: '../credits', isHide: true }
 	];
 
 	let linksVisible = false;
@@ -54,68 +52,63 @@
 	}
 </script>
 
-<div use:clickOutside on:click_outside={handleClickOutside}>
-	<div class="duration-400 {isAnimated ? 'pb-[100px]' : 'pb-16'}">
+<header use:clickOutside on:click_outside={handleClickOutside}>
+	<div class="duration-400 w-full {isAnimated ? 'pb-[100px]' : 'pb-16'}">
 		<div
-			class="duration-400 fixed top-0 z-50 flex w-full flex-col justify-center overflow-auto align-middle transition-all {isAnimated
+			class="duration-400 fixed top-0 z-50 w-full justify-center overflow-auto align-middle transition-all {isAnimated
 				? 'p-3'
-				: ''}"
+				: 'shadow-lg'}"
 		>
-			<div
-				class="duration-400 flex flex-row dark:bg-dark bg-white p-4 shadow-lg transition-all {isAnimated
+			<nav
+				class="duration-400 flex flex-row items-center gap-2 dark:bg-dark bg-white p-4 shadow-lg transition-all {isAnimated
 					? 'rounded-xl'
 					: 'dark:sm:bg-opacity-50 sm:bg-opacity-50 sm:backdrop-blur-md dark:sm:backdrop-blur-md'}"
 			>
-				<div class="flex w-1/2 flex-row items-center gap-1 duration-300 sm:gap-2" id="branding">
-					<button
-						id="logo"
-						class="duration-200 hover:scale-105 rounded-full active:scale-95"
-						on:click={handleClick}
-					>
-						<img
-							id="brandingLogo"
-							src="/logo.svg"
-							class="h-9 items-center rounded-full bg-dark_light bg-opacity-50 backdrop-blur-md sm:h-10"
-							alt="logo"
-							class:spin={spinning}
-							loading="lazy"
-						/>
-					</button>
-					<a
-						href="/"
-						id="brandingWoodmark"
-						class="rounded-lg p-2 text-base font-bold duration-200 hover:scale-105 text-black dark:text-white hover:bg-dark_light hover:bg-opacity-50 active:scale-95 sm:text-lg"
-						alt="branding woodmark"
-					>
-						User5522
-					</a>
-				</div>
-				<div class="flex w-1/2 justify-end gap-2">
+				<!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
+				<img
+					id="brandingLogo"
+					src="/logo.svg"
+					class="duration-200 bg-opacity-50 rounded-full h-9 bg-dark_light backdrop-blur-md sm:h-10 hover:scale-105 active:scale-95"
+					alt="logo"
+					class:spin={spinning}
+					loading="lazy"
+					on:click={handleClick}
+				/>
+				<a
+					href="/"
+					id="brandingWoodmark"
+					class="p-2 text-base font-bold text-black duration-200 rounded-lg hover:bg-opacity-50 dark:text-white hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
+				>
+					User5522
+				</a>
+				<div class="flex flex-row items-center gap-2 ml-auto mr-0">
 					{#each navItems as navItem}
 						<a
 							href={navItem.href}
-							class="{navItem.isPCHide ? 'sm:hidden' : 'sm:block'} {navItem.isHide
-								? 'hidden'
-								: 'block'}  rounded-lg p-2 text-base font-medium hover:bg-opacity-50 text-black dark:text-white duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
+							class="{navItem.isHide
+								? 'hidden sm:block'
+								: 'block sm:hidden'}  rounded-lg p-2 text-base font-semibold hover:bg-opacity-50 text-black dark:text-white duration-200 hover:scale-105 hover:bg-dark_light active:scale-95 sm:text-lg"
 						>
 							{navItem.name}
 						</a>
 					{/each}
 					<div class="block sm:hidden">
 						<button
-							class="items-center rounded-lg duration-200 hover:scale-105 hover:bg-opacity-50 hover:bg-dark_light active:scale-95"
+							class="items-center duration-200 rounded-lg hover:scale-105 hover:bg-opacity-50 hover:bg-dark_light active:scale-95"
 							on:click={() => (linksVisible = !linksVisible)}
 						>
-							<p class="rotate-90 p-2 text-xl">|||</p>
+							<p class="p-2 text-xl rotate-90">|||</p>
 						</button>
 					</div>
-					<div class="sm:block hidden self-center"><Theme /></div>
+					<div class="self-center hidden rounded-full sm:block"><Theme /></div>
 				</div>
-			</div>
+			</nav>
 		</div>
 	</div>
 	<div
-		class="{isAnimated ? 'px-3' : ''} duration-400 fixed z-50 w-full transition-all {linksVisible
+		class="{isAnimated
+			? 'px-3'
+			: ''} shadow-lg duration-400 fixed z-50 w-full transition-all {linksVisible
 			? 'block sm:hidden'
 			: 'hidden'}"
 		id="links-container"
@@ -130,15 +123,16 @@
 				<a
 					href={navMenuItem.href}
 					on:click={() => (linksVisible = !linksVisible)}
-					class="block rounded-xl p-2 dark:text-white text-black hover:bg-opacity-50 duration-200 hover:scale-102 hover:bg-dark_light active:scale-95"
+					class="block p-2 text-black duration-200 rounded-xl dark:text-white hover:bg-opacity-50 hover:scale-102 hover:bg-dark_light active:scale-95"
 				>
 					{navMenuItem.name}
 				</a>
 			{/each}
-			<Theme />
+
+			<div><Theme /></div>
 		</div>
 	</div>
-</div>
+</header>
 <span bind:this={observedSpan} />
 
 <style>
