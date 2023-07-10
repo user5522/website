a7fee9ca5d38c2a0977c4a792fc9dfe28d6f4ddd
Username * <oueslatim@pm.me>
2023-07-10 17:12:30 +0100

    rewrite the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,45 +1,29 @@
 <script>
-	import Navbar from '$lib/components/navbar.svelte';
-	import WavesStart from '$lib/components/wavesStart.svelte';
-	import Footer from '$lib/components/footer.svelte';
 	import { page } from '$app/stores';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
 	import '../app.css';
 
-	let layoutExcl = [
-		{ id: '/(links/beta' },
-		{ id: '/(links)/discord' },
-		{ id: '/(links)/github' },
-		{ id: '/(links)/reddit' },
-		{ id: '/(links)/sgsource' },
-		{ id: '/(links)/snaksource' },
-		{ id: '/(links)/source' },
-		{ id: '/(links)/steam' },
-		{ id: '/(links)/twitter' },
-		{ id: '/(links)/vbot' },
-		{ id: '/(links)/vbot/source' },
-		{ id: '/(links)/xbox' },
-		{ id: '/(links)/yt' },
-		{ id: '/(links)/u3d' },
-		{ id: '/accusations' }
-	];
+	let layoutExcl = [{ id: '/accusations' }, { id: '/projects/vbot' }];
 </script>
 
-{#if $page.error || layoutExcl.some((item) => $page.route.id.toLowerCase() == item.id)}
-	<div class="dark:bg-black dark:text-white bg-white text-black">
-		<slot />
-	</div>
-{:else}
-	<div
-		class="dark:bg-white bg-dark"
-		style="transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;"
-	>
-		<Navbar />
-		<WavesStart />
-	</div>
+<svelte:head>
+	<title>{$page.error || $page.route.id == '/' ? `` : $page.route.id}</title>
+</svelte:head>
 
-	<div class="init-div">
+{#if $page.error || layoutExcl.some((item) => $page.route.id.toLowerCase() == item.id)}
+	<main class="text-black bg-white dark:bg-black dark:text-white">
 		<slot />
-	</div>
+	</main>
+{:else}
+	<Navbar />
+
+	<main
+		class="flex flex-col items-center justify-center w-full max-w-5xl gap-3 px-5 pb-5 mx-auto pt-28 2xl:max-w-7xl"
+	>
+		<slot />
+	</main>
+
 	<Footer />
-	<div class="hidden 4xl:block text-xl">sorry, yer screen too big {':('}</div>
 {/if}
+<div class="hidden pl-2 text-xl 4xl:block">'pologies, but yer screen too big {':('}</div>
