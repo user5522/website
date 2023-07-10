34da89e2c73b24ebeac5007a2bd659570e560bec
Username * <oueslatim@pm.me>
2023-07-10 17:02:32 +0100

    less code i guess

--- src/lib/accusations/accusations.svelte
+++ src/lib/accusations/accusations.svelte
@@ -4,10 +4,8 @@
 	let titles = Data.map((item) => item.title);
 </script>
 
-<div class="flex flex-col items-center justify-center py-5">
-	<ul class="list-disc list-inside">
-		{#each titles as title}
-			<li>{title}</li>
-		{/each}
-	</ul>
-</div>
+<ul class="list-disc list-inside">
+	{#each titles as title}
+		<li>{title}</li>
+	{/each}
+</ul>
