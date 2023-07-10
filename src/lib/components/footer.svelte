4d8af6fb24a8703b6578566f9086179250e3f0ff
Username * <oueslatim@pm.me>
2023-07-10 17:04:02 +0100

    rewrite the footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,78 +1,31 @@
 <script>
+	import { socials } from '$lib/utilities/socials.js';
 	export let glitch = false;
 
 	let websiteLinks = [
 		{
-			name: 'Homepage',
+			name: 'Home',
 			href: '/'
-		},
-		{
-			name: 'Accusations',
-			href: '/accusations'
 		},
 		{
 			name: 'Projects',
 			href: '/projects'
 		},
 		{
-			name: 'Vbot (WIP)',
-			href: '/projects/vbot'
+			name: 'Collections',
+			href: '/collections'
 		},
 		{
 			name: 'Graphic designs',
 			href: '/projects/designs'
 		},
 		{
+			name: 'Accusations',
+			href: '/accusations'
+		},
+		{
 			name: 'Credits',
 			href: '/credits'
-		},
-		{
-			name: 'Collections',
-			href: '/collections'
-		}
-	];
-	let redirectLinks = [
-		{
-			name: 'Discord',
-			href: '/discord',
-			imgURL: '/logos/discord.svg',
-			imgAlt: 'Discord Logo'
-		},
-		{
-			name: 'Github',
-			href: '/github',
-			imgURL: '/logos/github.svg',
-			imgAlt: 'Github Logo'
-		},
-		{
-			name: 'Reddit',
-			href: '/reddit',
-			imgURL: '/logos/reddit.svg',
-			imgAlt: 'Reddit Logo'
-		},
-		{
-			name: 'Twitter',
-			href: '/twitter',
-			imgURL: '/logos/twitter.svg',
-			imgAlt: 'Twitter Logo'
-		},
-		{
-			name: 'Xbox',
-			href: '/xbox',
-			imgURL: '/logos/xbox.svg',
-			imgAlt: 'Xbox Logo'
-		},
-		{
-			name: 'Youtube',
-			href: '/yt',
-			imgURL: '/logos/youtube.svg',
-			imgAlt: 'Youtube Logo'
-		},
-		{
-			name: 'Invite Vbot',
-			href: '/vbot',
-			imgURL: '/vbot/vbot.svg',
-			imgAlt: 'Vbot Logo'
 		}
 	];
 	let testingLinks = [
@@ -82,72 +35,73 @@
 	];
 </script>
 
-<footer class=" bottom-0 w-full bg-true_gray p-5 text-white">
+<footer class="flex flex-col w-full gap-3 p-5 text-white bg-true_gray">
 	<div class="flex flex-row">
 		<div class="flex flex-col gap-5 sm:flex-row sm:gap-16">
-			<div class="flex flex-col">
-				<div class="font-semibold uppercase pb-2">user5522</div>
-				{#each websiteLinks as websiteLink}
-					<ul>
+			<div class="flex flex-col gap-2">
+				<div class="font-semibold uppercase">user5522</div>
+				<ul>
+					{#each websiteLinks as websiteLink}
 						<li>
 							<a href={websiteLink.href} class="text-blue-300">{websiteLink.name}</a>
 						</li>
-					</ul>
-				{/each}
+					{/each}
+				</ul>
 			</div>
-			<div class="flex flex-col">
-				<div class="font-semibold uppercase pb-2">links</div>
-				{#each redirectLinks as redirectLink}
-					<ul>
-						<li>
-							<a href={redirectLink.href} class="flex flex-row gap-1 text-blue-300">
-								<img
-									src={redirectLink.imgURL}
-									class="relative h-5 w-5 self-center"
-									alt={redirectLink.imgAlt}
-									loading="lazy"
-								/>
-								{redirectLink.name}
-							</a>
-						</li>
-					</ul>
-				{/each}
-			</div>
-			<div class="flex flex-col">
-				<div class="font-semibold uppercase pb-2">beta</div>
-				{#each testingLinks as testingLink}
-					<ul>
+			<div class="flex flex-col gap-2">
+				<div class="font-semibold uppercase">beta</div>
+				<ul>
+					{#each testingLinks as testingLink}
 						<li>
 							<a href={testingLink.href} class="text-blue-300">{testingLink.name}</a>
 						</li>
-					</ul>
-				{/each}
+					{/each}
+				</ul>
 			</div>
 		</div>
 
 		<img
 			src="/logo.svg"
 			alt="monochrome logo"
-			class="ml-auto h-28 w-28 rounded-full bg-light_true_gray grayscale sm:h-32 sm:w-32"
+			class="ml-auto rounded-full h-28 w-28 bg-light_true_gray grayscale sm:h-32 sm:w-32"
 			loading="lazy"
 		/>
 	</div>
 
-	<div class="pt-5 text-lighter_true_gray">
-		Found some <div
-			class="glitch inline-block bg-dark_light p-0.5 rounded-xl"
-			on:mouseenter={(glitch = true)}
-			on:mouseleave={(glitch = false)}
-		>
-			bugs
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div class="flex flex-col gap-3 sm:flex-row">
+		<div class="text-lighter_true_gray">
+			Found some <div
+				class="glitch inline-block bg-dark_light p-0.5 rounded-xl"
+				on:mouseenter={(glitch = true)}
+				on:mouseleave={(glitch = false)}
+			>
+				bugs
+			</div>
+			?
+			<a
+				href="https://github.com/user5522/website/issues"
+				target="blank"
+				class="underline dark:text-lighter_true_gray text-lighter_true_gray"
+				rel="noreferrer">report them by submitting an issue here!</a
+			>
 		</div>
-		?
-		<a
-			href="https://github.com/user5522/website/issues"
-			target="blank"
-			class="dark:text-lighter_true_gray text-lighter_true_gray underline"
-			rel="noreferrer">report them by submitting an issue here!</a
-		>
+		<div class="sm:mr-0 sm:ml-auto">
+			<ul class="flex flex-row justify-center gap-2">
+				{#each socials as social}
+					<li>
+						<a href={social.href} target="blank">
+							<img
+								src={social.imgURL}
+								class="self-center inline-block w-7 h-7"
+								alt="{social.name} Logo"
+								loading="lazy"
+							/>
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </footer>
 
