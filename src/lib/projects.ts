49bf092ad237d69b474c37070d6bab457a63bdda
Username * <oueslatim@pm.me>
2025-07-06 09:23:09 +0100

    change some wording

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,27 +1,27 @@
-export let projects: {link: string, name: string, description: string }[] = [
-    {
-        link:"https://github.com/user5522/timetable",
-        name: "Timetable",
-        description: "A flutter based material 3 designed timetable app used to manage school subjects and classes."
-    },
-    {
-        link: "https://github.com/user5522/website",
-        name: "This website",
-        description: "A svelte powered website/portfolio hosted on vercel."
-    },
-    {
-        link: "https://github.com/user5522/squaregame",
-        name: "SquareGame",
-        description: "A Unity 2D platformer game."
-    },
-    {
-        link: "https://github.com/user5522/u3d",
-        name: "u3d",
-        description: "A Unity 3D, supposedly fast paced, platformer game."
-    },
-    {
-        link: "https://github.com/user5522/snak",
-        name: "snak",
-        description: "A snake game made in rust using Bevy Engine."
-    }
+export let projects: { link: string; name: string; description: string }[] = [
+	{
+		link: 'https://github.com/user5522/timetable',
+		name: 'Timetable',
+		description: 'A Flutter based material 3 designed timetable android app for managing schedules.'
+	},
+	{
+		link: 'https://github.com/user5522/website',
+		name: 'website',
+		description: 'A Svelte powered website/portfolio hosted on vercel.'
+	},
+	{
+		link: 'https://github.com/user5522/snak',
+		name: 'snak',
+		description: 'A Bevy Engine snake game written in rust.'
+	},
+	{
+		link: 'https://github.com/user5522/squaregame',
+		name: 'SquareGame',
+		description: 'A Unity 2D platformer game.'
+	},
+	{
+		link: 'https://github.com/user5522/u3d',
+		name: 'u3d',
+		description: 'A Unity 3D, supposedly fast paced, platformer game.'
+	}
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 
 <h1>i'm User5522</h1>
 <p>
-	i make things that i think are cool, and often times i get inspired from other projects or
-	sometimes from everyday life.
+	i make cool, usually useful stuff that solves my problems, often inspired by other projects or
+	just stuff that doesn't exist yet / doesn't work the way i want it to.
 </p>
 <p>some of my projects include:</p>
 <ul>
@@ -15,13 +15,14 @@
 	{/each}
 </ul>
 <p>
-	interested in what i do? check out my <a href="https://github.com/user5522" target="_blank"
-		>GitHub</a
+	if you are interested in what i do, check out my <a
+		href="https://github.com/user5522"
+		target="_blank">GitHub</a
 	>.
 </p>
 
 <h2>Social</h2>
-<p>i'm usually not very active anywhere but whatever</p>
+<p>i'm not active everywhere but do always feel free to reach out</p>
 <ul>
 	{#each socials as social}
 		<li><a href={social.link} target="_blank">{social.name}</a>: {social.activity}</li>
